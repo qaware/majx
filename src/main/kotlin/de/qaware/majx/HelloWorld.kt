b939d38fd522f54c66445c75d65ff8259d4295bf
Claudius Boettcher <claudius.boettcher@gmail.com>
2017-07-12 20:21:14 +0200

    [#1] Gradle kotlin setup and some sample code to test coverage reports.

---
+++ src/main/kotlin/de/qaware/majx/HelloWorld.kt
@@ -0,0 +1,12 @@
+package de.qaware.majx
+
+class HelloWorld {
+
+    fun sayHello(formal: Boolean): String {
+        if (formal) {
+            return "Hello, World!"
+        } else {
+            return "Cheers, World!"
+        }
+    }
+}
---
+++ src/test/kotlin/de/qaware/majx/HelloWorldTest.kt
@@ -0,0 +1,11 @@
+package de.qaware.majx
+
+import org.junit.Test
+
+class HelloWorldTest {
+
+    @Test
+    fun testSayHello() {
+        HelloWorld().sayHello(false)
+    }
+}
