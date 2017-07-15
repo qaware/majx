f199d075572002367fce6a31c8808e5997b09374
Claudius Boettcher <claudius.boettcher@gmail.com>
2017-07-15 19:37:09 +0200

    [#1] Added overload without reason param.

--- src/main/kotlin/de/qaware/majx/Majx.kt
+++ src/main/kotlin/de/qaware/majx/Majx.kt
@@ -3,11 +3,14 @@ package de.qaware.majx
 import com.fasterxml.jackson.databind.JsonNode
 import convertToJsonNode
 
-fun assertJsonMatches(reason: String?, actual: String, pattern: String, mustacheScope: Any?) =
+fun assertJsonMatches(actual: String, pattern: String, mustacheScope: Any? = null) =
+        assertJsonMatches(null, actual, pattern, mustacheScope)
+
+fun assertJsonMatches(reason: String?, actual: String, pattern: String, mustacheScope: Any? = null) =
         assertJsonMatches(reason,
                 convertToJsonNode(actual),
                 convertToJsonNode(pattern),
                 mustacheScope)
 
-fun assertJsonMatches(reason: String?, actual: JsonNode, pattern: JsonNode, mustacheScope: Any?) =
+private fun assertJsonMatches(reason: String?, actual: JsonNode, pattern: JsonNode, mustacheScope: Any?) =
         JsonMatcher(mustacheScope).assertMatches(reason, actual, pattern)
