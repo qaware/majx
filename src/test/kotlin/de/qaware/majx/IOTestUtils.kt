853af9988b6c5055feb483ad8192e687132ae8ef
Claudius Boettcher <claudius.boettcher@gmail.com>
2017-07-15 19:42:23 +0200

    [#2] Added tests for object mismatches.

--- src/test/kotlin/de/qaware/majx/IOTestUtils.kt
+++ src/test/kotlin/de/qaware/majx/IOTestUtils.kt
@@ -8,25 +8,23 @@ import java.io.InputStream
 /**
  * Read file from resources as string using UTF 8 encoding.
  *
- * @param fileName File name
- * @return File content
- * @throws IOException If file could not be read
+ * @param fileName The file name.
+ * @return The file content as string.
+ * @throws IOException If file could not be read.
  */
 @Throws(IOException::class)
-fun readFile(fileName: String): String {
-    openResource(fileName).use { stream -> return IOUtils.toString(stream, "UTF-8") }
+fun readFile(fileName: String): String = openResource(fileName).use { stream ->
+    return IOUtils.toString(stream, "UTF-8")
 }
 
 /**
- * Open stream to file in resources
+ * Open stream to file in resources.
 
- * @param fileName File name
- * @return Stream. Has to be closed by caller
- * @throws IOException If file could not be read
+ * @param fileName The file name.
+ * @return The stream. Has to be closed by caller.
+ * @throws IOException If file could not be read.
  */
 @Throws(IOException::class)
-private fun openResource(fileName: String): InputStream {
-    val stream = Thread.currentThread().contextClassLoader.getResourceAsStream(fileName) ?:
-            throw IllegalArgumentException(String.format("Resource %s not found", fileName))
-    return stream
-}
+private fun openResource(fileName: String): InputStream =
+        Thread.currentThread().contextClassLoader.getResourceAsStream(fileName) ?:
+                throw IllegalArgumentException(String.format("Resource %s not found", fileName))
--- src/test/kotlin/de/qaware/majx/MajxTest.kt
+++ src/test/kotlin/de/qaware/majx/MajxTest.kt
@@ -1,23 +1,54 @@
 package de.qaware.majx
 
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.`is`
 import org.junit.Test
+import kotlin.test.fail
 
 class MajxTest {
 
     @Test
-    fun testEmptyObject() {
-        val actual = readFile("exact/empty/object/actual.json")
-        val pattern = readFile("exact/empty/object/pattern.json")
+    fun testEmptyObjectsSuccess() {
+        val actual = readFile("exact/empty/object/success/actual.json")
+        val pattern = readFile("exact/empty/object/success/pattern.json")
 
-        assertJsonMatches(null, actual, pattern, null)
+        assertJsonMatches(actual, pattern)
     }
 
     @Test
-    fun testEmptyArray() {
+    fun testUnexpectedInObject() {
+        val actual = readFile("exact/empty/object/error/unexpected/actual.json")
+        val pattern = readFile("exact/empty/object/error/unexpected/pattern.json")
+        val expectedMessage = readFile("exact/empty/object/error/unexpected/expectedMessage.txt")
+
+        try {
+            assertJsonMatches(actual, pattern)
+            fail()
+        } catch (ex: AssertionError) {
+            assertThat(ex.message, `is`(expectedMessage))
+        }
+    }
+
+    @Test
+    fun testMissingInObject() {
+        val actual = readFile("exact/empty/object/error/missing/actual.json")
+        val pattern = readFile("exact/empty/object/error/missing/pattern.json")
+        val expectedMessage = readFile("exact/empty/object/error/missing/expectedMessage.txt")
+
+        try {
+            assertJsonMatches(actual, pattern)
+            fail()
+        } catch (ex: AssertionError) {
+            assertThat(ex.message, `is`(expectedMessage))
+        }
+    }
+
+    @Test
+    fun testEmptyArraysSuccess() {
         val actual = readFile("exact/empty/array/actual.json")
         val pattern = readFile("exact/empty/array/pattern.json")
 
-        assertJsonMatches(null, actual, pattern, null)
+        assertJsonMatches(actual, pattern)
     }
 
     @Test
@@ -25,7 +56,7 @@ class MajxTest {
         val actual = readFile("mustache/actual.json")
         val pattern = readFile("mustache/pattern.json")
 
-        assertJsonMatches(null, actual, pattern, mapOf(
+        assertJsonMatches(actual, pattern, mapOf(
                 Pair("mustacheVar1", "mustacheValue1"),
                 Pair("mustacheVar2", "mustacheValue2")
         ))
