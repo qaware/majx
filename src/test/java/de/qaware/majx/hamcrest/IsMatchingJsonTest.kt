7b3ca693deec2c8180a6def480d82f8cb0a3968e
vlow <vlow+git@turbocache3000.de>
2020-04-30 22:30:16 +0200

    Add mismatch test case

--- src/test/java/de/qaware/majx/hamcrest/IsMatchingJsonTest.kt
+++ src/test/java/de/qaware/majx/hamcrest/IsMatchingJsonTest.kt
@@ -4,11 +4,15 @@ import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Test
 
 
-
 internal class IsMatchingJsonTest {
 
     @Test
     fun matchesSafely() {
         assertThat("{ \"foo\" : \"bar\" }", matchesJson("{ \"foo\" : \"...\" }"))
+    }
+
+    @Test(expected = AssertionError::class)
+    fun mismatch() {
+        assertThat("{ \"foo\" : \"bar\" }", matchesJson("{ \"baz\" : \"...\" }"))
     }
 }
